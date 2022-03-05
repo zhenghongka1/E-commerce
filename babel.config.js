8d92d6066b13df81125a50d2d8ac10074069e0c7
郑鸿凯 <8694910+zheng-hongkai@user.noreply.gitee.com>
2022-03-05 17:53:00 +0800

    忘记提交了我曹

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+// 项目发布阶段需要用到的babel插件
+const prodPlugin = []
+
+if (process.env.NODE_ENV === 'production') {
+  prodPlugin.push("transform-remove-console")
+}
+
 module.exports = {
   presets: [
     '@vue/app',
@@ -10,6 +17,9 @@ module.exports = {
         "libraryName": "element-ui",
         "styleLibraryName": "theme-chalk"
       }
-    ]
+    ],
+    // 发布产品时候用到的插件数组
+    ...prodPlugin,
+    '@babel/plugin-syntax-dynamic-import' 
   ]
 }
--- src/main-dev.js
+++ src/main-dev.js
@@ -16,6 +16,10 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+// 引入加载进度条对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 // axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
 
 // axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
@@ -24,11 +28,19 @@ axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
 
 
 
-axios.interceptors.request.use(config =>{
+axios.interceptors.request.use(config => {
+  // 在request发起请求的时候加载进度条
+  NProgress.start();
   // console.log(config);
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 必须return出来
   return config
+})
+axios.interceptors.response.use(config => {
+  // 在response发起请求的时候加载进度条
+  NProgress.done();
+  return config
+
 })
 
 Vue.prototype.$http = axios
@@ -39,25 +51,25 @@ Vue.config.productionTip = false
 Vue.use(less)
 // Vue.use(ElementUI)
 
-Vue.component('tree-table',TreeTable)
+Vue.component('tree-table', TreeTable)
 
 // 将富文本编辑器注册为全局可用的组件
 Vue.use(VueQuillEditor)
 
 // 时间过滤器
-Vue.filter('dateFormate',function(time){
-const date = new Date(time)
-const years = date.getFullYear()
-// padStart第一个代表总长度为多少位，第二个表示如果不足多少位，用来填充
-const months = (date.getMonth() + 1 + '').padStart(2,'0')
-const days = (date.getDay() + '').padStart(2,'0')
+Vue.filter('dateFormate', function (time) {
+  const date = new Date(time)
+  const years = date.getFullYear()
+  // padStart第一个代表总长度为多少位，第二个表示如果不足多少位，用来填充
+  const months = (date.getMonth() + 1 + '').padStart(2, '0')
+  const days = (date.getDay() + '').padStart(2, '0')
 
-const hours = (date.getHours() + '').padStart(2,'0')
-const minutes = (date.getMinutes() + '').padStart(2,'0')
+  const hours = (date.getHours() + '').padStart(2, '0')
+  const minutes = (date.getMinutes() + '').padStart(2, '0')
 
-const seconds = (date.getSeconds() + '').padStart(2,'0')
+  const seconds = (date.getSeconds() + '').padStart(2, '0')
 
-return `${years}-${months}-${days} ${hours}:${minutes}:${seconds}`
+  return `${years}-${months}-${days} ${hours}:${minutes}:${seconds}`
 })
 
 new Vue({
